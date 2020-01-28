9034c6fe43bb1d7c421a27a14957a23a9cfb03be
Cory House <housecor@gmail.com>
2020-01-28 11:40:03 -0600

    add css modules

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Card.module.scss";
 
 function Card(props) {
-  return <div>{props.person.lastName}</div>;
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.header}>{props.person.lastName}</h2>
+      Phone: {props.person.phone}
+      <br />
+      Zip: {props.person.zip}
+    </div>
+  );
 }
 
 export default Card;
